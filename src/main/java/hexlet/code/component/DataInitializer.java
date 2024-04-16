38f5f7715d5e108f3e2aba841392a1493ec05951
sheveleves <shevelve@gmail.com>
2024-04-16 11:47:19 +0500

    step3. task 3 adding DataInitializer with setup of adminUser

---
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -0,0 +1,41 @@
+package hexlet.code.component;
+
+import hexlet.code.model.User;
+import hexlet.code.repository.UserRepository;
+import net.datafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements ApplicationRunner {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private Faker faker;
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        String email = "hexlet@example.com";
+        User user = new User();
+        user.setEmail(email);
+        String encodedPassword = passwordEncoder.encode("qwerty");
+        user.setPassword(encodedPassword);
+        userRepository.save(user);
+
+
+        for (int i = 0; i++ < 10;) {
+            user = new User();
+            user.setEmail(faker.internet().emailAddress());
+            user.setFirstName(faker.name().firstName());
+            user.setLastName(faker.name().lastName());
+            encodedPassword = passwordEncoder.encode("12345");
+            user.setPassword(encodedPassword);
+            userRepository.save(user);
+        }
+    }
+}
---
+++ src/main/java/hexlet/code/handler/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package hexlet.code.handler;
+
+import hexlet.code.exception.ResourceNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+        List<String> errors = ex.getBindingResult().getFieldErrors()
+                .stream().map(FieldError::getDefaultMessage).collect(Collectors.toList());
+        logger.info(getErrorsMap(errors));
+
+        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    private Map<String, List<String>> getErrorsMap(List<String> errors) {
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
+        return errorResponse;
+    }
+}
