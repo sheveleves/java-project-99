5c3a3db526ca7034cf83792f8f4985bafdf2347b
sheveleves <shevelve@gmail.com>
2024-04-17 11:50:02 +0500

    adding validation

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -4,8 +4,10 @@ import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AppApplication {
 
     public static void main(String[] args) {
--- src/main/java/hexlet/code/model/User.java
+++ src/main/java/hexlet/code/model/User.java
@@ -1,12 +1,17 @@
 package hexlet.code.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 
@@ -14,14 +19,19 @@ import java.time.LocalDate;
 @Table(name = "users")
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EntityListeners(AuditingEntityListener.class)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
+    @CreatedDate
     private LocalDate createdAt;
+    @LastModifiedDate
     private LocalDate updatedAt;
 }
