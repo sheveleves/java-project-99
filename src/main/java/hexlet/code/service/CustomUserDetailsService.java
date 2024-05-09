43e8990f7581efc7555ad090a5dfc8d1da5679eb
sheveleves <shevelve@gmail.com>
2024-05-09 11:42:46 +0500

    step4. implementation UserDetailsManager

---
+++ src/main/java/hexlet/code/service/CustomUserDetailsService.java
@@ -0,0 +1,64 @@
+package hexlet.code.service;
+
+import hexlet.code.model.User;
+import hexlet.code.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CustomUserDetailsService implements UserDetailsManager {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format("User with %s not found", email)));
+    }
+
+    @Override
+    public void createUser(UserDetails userData) {
+        if (userExists(userData.getUsername())) {
+            log.info(String.format("Attempt to create a user with the same email - %s", userData.getUsername()));
+            return;
+        }
+        User user = new User();
+        user.setEmail(userData.getUsername());
+        user.setPasswordDigest(passwordEncoder.encode(userData.getPassword()));
+        userRepository.save(user);
+    }
+
+    @Override
+    public void updateUser(UserDetails user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateUser'");
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteUser'");
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'changePassword'");
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        return userRepository.findByEmail(username).isPresent();
+    }
+}
