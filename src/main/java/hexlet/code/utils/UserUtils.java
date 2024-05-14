649e362b4374e3b31c85fe34db09e592452801db
sheveleves <shevelve@gmail.com>
2024-05-14 09:15:37 +0500

    step4. add UserUtils class

---
+++ src/main/java/hexlet/code/utils/UserUtils.java
@@ -0,0 +1,27 @@
+package hexlet.code.utils;
+
+import hexlet.code.model.User;
+import hexlet.code.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class UserUtils {
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+        String username = authentication.getName();
+        return userRepository.findByEmail(username).get();
+    }
+
+    public boolean isUserTheSameFromAuthentication(long id) {
+        return getCurrentUser().getId() == id;
+    }
+}
