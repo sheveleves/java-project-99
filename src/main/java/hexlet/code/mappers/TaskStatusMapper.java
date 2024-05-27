bd1f2e115ec3c95fd91999638905f4406a8d7945
sheveleves <shevelve@gmail.com>
2024-05-27 10:34:41 +0500

    step6. add TaskStatusMapper

---
+++ src/main/java/hexlet/code/mappers/TaskStatusMapper.java
@@ -0,0 +1,23 @@
+package hexlet.code.mappers;
+
+import hexlet.code.dto.TaskStatusCreateDTO;
+import hexlet.code.dto.TaskStatusDTO;
+import hexlet.code.dto.TaskStatusUpdateDTO;
+import hexlet.code.model.TaskStatus;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        uses = {JsonNullableMapper.class},
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class TaskStatusMapper {
+    public abstract TaskStatus map(TaskStatusCreateDTO taskStatusCreateDTO);
+    public abstract TaskStatusDTO map(TaskStatus taskStatus);
+    public abstract void map(TaskStatusUpdateDTO taskStatusUpdateDTO, @MappingTarget TaskStatus taskStatus);
+}
