8200e188b98cc7be389619a508cebe8b486debf6
sheveleves <shevelve@gmail.com>
2024-05-27 16:12:37 +0500

    step6. add defaulted TaskStatuses in the DataInitializer

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -1,6 +1,8 @@
 package hexlet.code.component;
 
+import hexlet.code.model.TaskStatus;
 import hexlet.code.model.User;
+import hexlet.code.repository.TaskStatusRepository;
 import hexlet.code.repository.UserRepository;
 import net.datafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,24 +11,45 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
 public class DataInitializer implements ApplicationRunner {
     @Autowired
     private UserRepository userRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
 
     @Autowired
     private Faker faker;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         String email = "hexlet@example.com";
         User user = new User();
-        user.setEmail(email);
         String encodedPassword = passwordEncoder.encode("qwerty");
-        user.setPasswordDigest(encodedPassword);
-        userRepository.save(user);
+        if (userRepository.findByEmail(email).isEmpty()) {
+            user.setEmail(email);
+            user.setPasswordDigest(encodedPassword);
+            userRepository.save(user);
+        }
 
+        Map<String, String> taskStatuses = Map.of(
+                "Draft", "draft",
+                "ToReview", "to_review",
+                "ToBeFixed", "to_be_fixed",
+                "ToPublish", "to_publish",
+                "Published", "published"
+        );
+
+        taskStatuses.forEach((name, slug) -> {
+            if (taskStatusRepository.findBySlug(slug).isEmpty()) {
+                TaskStatus taskStatus = new TaskStatus(name, slug);
+                taskStatusRepository.save(taskStatus);
+            }
+        });
 
         for (int i = 0; i++ < 10;) {
             user = new User();
