201e175964e310756ff29203e1d8a27588ac0c58
sheveleves <shevelve@gmail.com>
2024-06-04 21:01:12 +0500

    step7. add TaskController

---
+++ src/main/java/hexlet/code/controller/TaskController.java
@@ -0,0 +1,58 @@
+package hexlet.code.controller;
+
+import hexlet.code.dto.TaskCreateDTO;
+import hexlet.code.dto.TaskDTO;
+import hexlet.code.dto.TaskUpdateDTO;
+import hexlet.code.service.TaskService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@AllArgsConstructor
+public class TaskController {
+    private final TaskService taskService;
+
+    @GetMapping(path = "")
+    public ResponseEntity<List<TaskDTO>> index() {
+        var taskStatuses = taskService.getAllTasks();
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(taskStatuses.size()))
+                .body(taskStatuses);
+    }
+
+    @GetMapping(path = "/{id}")
+    public TaskDTO getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    @PostMapping(path = "")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TaskDTO createTask(@Valid @RequestBody TaskCreateDTO taskCreateDTO) {
+        return taskService.createTask(taskCreateDTO);
+    }
+
+    @PutMapping(path = "/{id}")
+    public TaskDTO updateTaskStatus(@Valid @RequestBody TaskUpdateDTO taskUpdateDTO, @PathVariable Long id) {
+        return taskService.updateTask(taskUpdateDTO, id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
+}
