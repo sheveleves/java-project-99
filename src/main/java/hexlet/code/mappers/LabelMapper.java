7c6c79285afa07358e990ef7f97513615d6b7222
sheveleves <shevelve@gmail.com>
2024-06-10 16:48:17 +0500

    step8. add LabelMapper

---
+++ src/main/java/hexlet/code/mappers/LabelMapper.java
@@ -0,0 +1,23 @@
+package hexlet.code.mappers;
+
+import hexlet.code.dto.LabelCreateDTO;
+import hexlet.code.dto.LabelDTO;
+import hexlet.code.dto.LabelUpdateDTO;
+import hexlet.code.model.Label;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        uses = {JsonNullableMapper.class},
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class LabelMapper {
+    public abstract LabelDTO map(Label label);
+    public abstract Label map(LabelCreateDTO labelCreateDTO);
+    public abstract void update(LabelUpdateDTO labelUpdateDTO, @MappingTarget Label label);
+}
